37a7f6a80bf8c763bd505d7627faf56c590f3d60
ShehanRajapaksha <shehan.rajapaksha2002@gmail.com>
2025-03-13 13:31:13 +0530

    Add mobile overlay component + UsercCard + Search results component - #28

--- src/Views/Home.jsx
+++ src/Views/Home.jsx
@@ -1,36 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import GroupCard from '../Components/GroupCard';
 import HeaderProfile from '../Components/HeaderProfile';
 import CreateGroupModal from '../Components/CreateGroup';
 import YourBalanceCard from '../Components/YourBalanceCard';
 import AddExpensePopup from '../Components/AddExpensePopup';
 import { Input } from "@material-tailwind/react";
+import MobileOverlay from '../Components/MobileOverlay';
 
 
 const Home = () => {
-    return (
-        
-    <div>
-    <HeaderProfile />
-        <YourBalanceCard />
-        <AddExpensePopup />
-        <div className="flex w-72 flex-col gap-6">
-            <Input variant="static" label="Static" placeholder="Static" />
-            <Input variant="standard" label="Standard" placeholder="Standard"/>
-            <Input variant="outlined" label="Outlined" placeholder="Outlined"/>
-        </div>
-        
-        <div style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gap: '10px' }}>
-            <GroupCard />
-            <GroupCard />
-            <GroupCard/>
+    const [isMobile, setIsMobile] = useState(false);
 
-            <GroupCard />
-            <GroupCard />
-            <GroupCard/>    
+    useEffect(() => {
+        const checkScreenSize = () => {
+            setIsMobile(window.matchMedia("(max-width: 768px)").matches);
+        };
+
+        checkScreenSize();
+        window.addEventListener("resize", checkScreenSize);
+
+        return () => window.removeEventListener("resize", checkScreenSize);
+    }, []);
+
+    return (
+
+        <div>
+            <HeaderProfile />
+            <YourBalanceCard />
+            <AddExpensePopup />
+            <div className="flex w-72 flex-col gap-6">
+                <Input variant="static" label="Static" placeholder="Static" />
+                <Input variant="standard" label="Standard" placeholder="Standard" />
+                <Input variant="outlined" label="Outlined" placeholder="Outlined" />
+            </div>
+
+            <div style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gap: '10px' }}>
+                <GroupCard />
+                <GroupCard />
+                <GroupCard />
+
+                <GroupCard />
+                <GroupCard />
+                <GroupCard />
+            </div>
+            {/* <CreateGroupModal /> */}
+            {isMobile && <MobileOverlay />}
         </div>
-        {/* <CreateGroupModal /> */}
-    </div>
     );
 };
 
