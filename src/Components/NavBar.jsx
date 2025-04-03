984bc84ca7adce43aa179ba7809843d93ff08e5a
Saradi140 <139766725+Saradi140@users.noreply.github.com>
2025-04-03 21:03:35 +0530

    Update NavBar.jsx

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import LogoutButton from "./LogoutButton";
+
+
 
 
 import settleImg from '../images/settle.png';
@@ -10,33 +13,37 @@ import activityImg from '../images/activity.png';
 
 const Navbar = () => {
   return (
-    <div className="fixed w-20 h-full top-0 bg-[#f1f2f9] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] border">
+    <div className="fixed w-14 h-full top-0 bg-[#f1f2f9] shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] border">
       <nav className="w-full flex-shrink-0 bg-[#F1F2F9] flex flex-col items-center py-4">
-        <Link to="/settle" className="w-8 h-8 relative overflow-hidden top-[120px]">
+        <Link to="/settle" className="w-7 h-7 relative overflow-hidden top-[60px]">
           <img src={settleImg} alt="Settle" />
         </Link>
-        <Link to="/cgroup" className="w-8 h-8 relative overflow-hidden top-[194px]">
+        <Link to="/cgroup" className="w-7 h-7 relative overflow-hidden top-[100px]">
           <img src={cgroupImg} alt="Cgroup" />
         </Link>
 
         {/* White Circle  */}
-        <div className="absolute left-8 top-[335px] w-[85px] h-[100px] bg-white rounded-full"></div>
+        <div className="absolute left-8 top-[220px] w-[80px] h-[80px] bg-white rounded-full"></div>
 
         {/* Blue Circle */}
         <Link
           to="/addexpense"
-          className="absolute left-[45px] top-[343px] w-[70px] h-[80px] bg-[#040b2b] text-white flex items-center justify-center rounded-full shadow-md border-2 border-white"
+          className="absolute left-[45px] top-[229px] w-[64px] h-[64px] bg-[#040b2b] text-white flex items-center justify-center rounded-full shadow-md border-2 border-white"
         >
-          <img src={addExpenseImg} alt="Add Expense" className="w-7 h-7 filter invert" />
+          <img src={addExpenseImg} alt="Add Expense" className="w-6 h-6 filter invert" />
 
         </Link>
 
-        <Link to="/add" className="w-8 h-8 relative overflow-hidden top-[434px]">
+        <Link to="/add" className="w-7 h-7 relative overflow-hidden top-[270px]">
           <img src={addImg} alt="Add" />
         </Link>
-        <Link to="/activity" className="w-8 h-8 relative overflow-hidden top-[508px]">
+        <Link to="/activity" className="w-7 h-7 relative overflow-hidden top-[310px] left-[-5px]">
           <img src={activityImg} alt="Activity" />
         </Link>
+        
+        <div className="w-10 h-8 relative overflow-hidden top-[480px]">
+          <LogoutButton />
+        </div>
       </nav>
     </div>
   );
