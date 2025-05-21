e88ad96e38b570b202f64e1a581850fed9d3ff6a
thanura mendis <thanuramendis02@gmail.com>
2025-05-21 19:11:25 +0530

    Logout button #32

--- App/src/Components/LogoutButton.jsx
+++ App/src/Components/LogoutButton.jsx
@@ -1,18 +1,23 @@
 import { useNavigate } from "react-router-dom";
 import logoutImg from "../images/logout.png"; 
+import { useAuthContext } from "@asgardeo/auth-react"; // Import Asgardeo authentication hook
+
 
 const LogoutButton = () => {
+    const { state, signIn, signOut, getAccessToken } = useAuthContext();
+
     const navigate = useNavigate();
 
-    const handleLogout = () => {
+    // const handleLogout = () => {
         
-        localStorage.removeItem("authToken"); 
-        navigate("/login"); 
-    };
+    //     localStorage.removeItem("authToken"); 
+    //     navigate("/login"); 
+    // };
 
     return (
         <button 
-            onClick={handleLogout} 
+            onClick={() => signIn()} 
+
             className="w-full h-full flex justify-center items-center p-2 transition-all duration-300 ease-in-out hover:bg-gray-200 rounded-lg"
         >
             <img
